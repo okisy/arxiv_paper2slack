ea69102e2686b87e139f6d48bd8e82e57a4eca55
okisy <32443681+okisy@users.noreply.github.com>
2025-08-19 16:04:28 +0900

    動いてないけどとりあえず関連ファイルをコミット

---
+++ cdk-lambda-app/test/cdk-lambda-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdaApp from '../lib/cdk-lambda-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lambda-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdaApp.CdkLambdaAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
